22c68f7795c4fc7072c3ad44160d0acea6647ac7
Shrisha Radhakrishna <shrisha@gmail.com>
2015-05-04 11:51:32 -0700

    apply some twitter bootstrap love

--- src/main/java/com/shrisha/autoclient/web/MainController.java
+++ src/main/java/com/shrisha/autoclient/web/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
-        return "index";
+        return trips(model);
     }
 
     @RequestMapping(value = "/automatic/trips", method = RequestMethod.GET)
